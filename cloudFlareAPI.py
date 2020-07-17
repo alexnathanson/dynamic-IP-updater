526cfd66cd28da6d3febf8c45697f41fac96e9c6
alexnathanson <alex@alexnathanson.com>
2020-07-17 12:44:43 -0400

    pools

---
+++ cloudFlareAPI.py
@@ -0,0 +1,29 @@
+'''
+Dynamic DNS with Cloudflare on Raspberry Pi server
+
+Updates dynamic IP
+
+1) check public IP
+2) compare with previous stored public IP
+3) check Cloud Flare origins
+4) if IP different, update Cloud Flare origin
+'''
+
+import requests
+
+
+
+headers = {
+    'X-Auth-Email': '',
+    'X-Auth-Key': '',
+    'Content-Type': 'application/json',
+}
+
+data = {}
+
+response = requests.get('https://api.cloudflare.com/client/v4/user/load_balancers/pools', headers=headers)
+
+
+print response
+
+
